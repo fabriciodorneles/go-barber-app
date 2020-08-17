a717ba59815ac0feb860dbe3739230a0e0a702fd
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-08-17 19:15:27 -0300

    finalizing create appointment

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import {
+    Title,
+    Container,
+    AppointmentCreatedInfo,
+    OkButton,
+    OkButtonText,
+} from './styles';
 
-import { Title } from './styles';
+interface RouteParams {
+    date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
+    const { reset } = useNavigation();
+    const { params } = useRoute();
+
+    const routeParams = params as RouteParams;
+
+    const formattedDate = useMemo(() => {
+        return format();
+    }, []);
+
+    const handleOkPressed = useCallback(() => {
+        reset({
+            routes: [{ name: 'Dashboard' }],
+            index: 0,
+        });
+    }, [reset]);
     return (
-        <>
-            <View>
-                <Title>Você está logado.</Title>
-            </View>
-        </>
+        <Container>
+            <Icon name="check" size={80} color="#04d361" />
+            <Title>Agendamento Concluído</Title>
+            <AppointmentCreatedInfo>
+                Terça, dia 14 de março de 2020 às 12:00
+            </AppointmentCreatedInfo>
+            <AppointmentCreatedInfo>com Diego Fernandes</AppointmentCreatedInfo>
+            <OkButton onPress={handleOkPressed}>
+                <OkButtonText>Ok</OkButtonText>
+            </OkButton>
+        </Container>
     );
 };
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
@@ -11,6 +12,13 @@ import {
     ProfileButton,
     UserAvatar,
     ProvidersList,
+    ProvidersListTitle,
+    ProviderContainer,
+    ProviderAvatar,
+    ProviderInfo,
+    ProviderName,
+    ProviderMeta,
+    ProviderMetaText,
 } from './styles';
 
 interface SignInFormData {
@@ -21,7 +29,7 @@ interface SignInFormData {
 export interface Provider {
     id: string;
     name: string;
-    avatar: string;
+    avatar_url: string;
 }
 
 const Dashboard: React.FC = () => {
@@ -32,13 +40,20 @@ const Dashboard: React.FC = () => {
     const navigateToProfile = useCallback(() => {
         // navigate('Profile');
         signOut();
-    }, []);
+    }, [signOut]);
 
     useEffect(() => {
         api.get('providers').then(response => {
             setProviders(response.data);
         });
     }, []);
+
+    const navigateToCreateAppointment = useCallback(
+        (providerId: string) => {
+            navigate('CreateAppointment', { providerId });
+        },
+        [navigate],
+    );
     return (
         <Container>
             <Header>
@@ -54,7 +69,33 @@ const Dashboard: React.FC = () => {
             <ProvidersList
                 data={providers}
                 keyExtractor={provider => provider.id}
-                renderItem={({ item }) => <UserName>{item.name}</UserName>}
+                ListHeaderComponent={
+                    <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+                }
+                renderItem={({ item: provider }) => (
+                    <ProviderContainer
+                        onPress={() => navigateToCreateAppointment(provider.id)}
+                    >
+                        <ProviderAvatar source={{ uri: provider.avatar_url }} />
+                        <ProviderInfo>
+                            <ProviderName>{provider.name}</ProviderName>
+                            <ProviderMeta>
+                                <Icon
+                                    name="calendar"
+                                    size={14}
+                                    color="#ff9000"
+                                />
+                                <ProviderMetaText>
+                                    Segunda à Sexta
+                                </ProviderMetaText>
+                            </ProviderMeta>
+                            <ProviderMeta>
+                                <Icon name="clock" size={14} color="#ff9000" />
+                                <ProviderMetaText>8h às 18h</ProviderMetaText>
+                            </ProviderMeta>
+                        </ProviderInfo>
+                    </ProviderContainer>
+                )}
             />
         </Container>
     );
